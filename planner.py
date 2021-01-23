e6beccfdb832a86e4f25783243c55770f475803b
Burton Johnsey <git@tonjohn.com>
2021-01-23 13:14:47 -0800

    move functions into their own file in preperation for tests
    if we import the methods into tests from main, the import statement will run the app which is not desired.

---
+++ planner.py
@@ -0,0 +1,47 @@
+import random
+
+def ask_user(tasks):
+
+    print('')
+    answers_left = 7
+    while answers_left > 0:
+        print('What do you need to do this week? Input tasks separated by a comma then a space. ')
+        answers = input('Enter: ').split(', ')
+        tasks += answers
+        answer_count = len(answers)
+        if len(answers) < 7:
+            answers_left -= answer_count
+            print('You still have ' + str(answers_left) + ' more tasks to put in.')
+            print('')
+        elif len(answers) >= 7:
+            break
+    return tasks
+
+def combine_lists(weekdays, tasks):
+
+    def task_list():
+        for i in range(7):
+            print(weekdays[i] + ': ' + task_copy[i])
+
+    task_copy = tasks.copy()
+    random.shuffle(task_copy)
+    reroll = True
+
+    print('')
+    print('Here\'s your task assignment for the week:')
+    print('')
+
+    task_list()
+
+    while reroll == True:
+        print('')
+        print('Do you want to re-roll the results?')
+        reroll_ask = input('Enter (Y) or (N): ').upper()
+        if reroll_ask == ('Y'):
+            print('')
+            random.shuffle(task_copy)
+            task_list()
+        elif reroll_ask == ('N'):
+            break
+        else:
+            print('Input invalid. Please try again.')
